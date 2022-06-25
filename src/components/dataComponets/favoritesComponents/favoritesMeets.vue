0802f87af8649c90753c28c89f48278672126cc6
ali.mohsenpour <alimpp766@gmail.com>
2022-06-25 11:17:05 -0700

    conditinal favorites components

---
+++ src/components/dataComponets/favoritesComponents/favoritesMeets.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="favoritesMeets">
+    <h1>Meets</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/dataComponets/favoritesDataComponent.vue
+++ src/components/dataComponets/favoritesDataComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="favoritesData">
     <div class="container">
+
       <div class="row">
         <div class="col-lg-6">
           <redCart class="mt-3"/>
@@ -9,14 +10,24 @@
            <blueCart class="mt-3"/>
          </div>
       </div>
+
       <div class="col-lg-12">
-        <button class="btn btn-info mt-3">Projects</button>
+        <button class="btn btn-info mt-3" @click="isProjects">Projects</button>
         <span class="pannelBadge mx-2">20</span>
-        <button class="btn btn-warning mt-3">Meet</button>
+        <button class="btn btn-warning mt-3" @click="isMeets">Meet</button>
         <span class="pannelBadge mx-2">20</span>
-        <button class="btn btn-primary mt-3">Posts</button>
+        <button class="btn btn-primary mt-3" @click="isPosts">Posts</button>
         <span class="pannelBadge mx-2">20</span>
       </div>
+
+      <div class="row">
+        <div class="col-lg-12">
+           <favoritesProjects v-if="projects"/>
+           <favoritesPosts v-if="posts"/>
+           <favoritesMeets v-if="meets"/>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -24,8 +35,33 @@
 <script>
 import redCart from '../cardComponents/redCart.vue'
 import blueCart from '../cardComponents/blueCart.vue'
+import favoritesProjects from '../dataComponets/favoritesComponents/favoritesProjects.vue'
+import favoritesPosts from '../dataComponets/favoritesComponents/favoritesPosts.vue' 
+import favoritesMeets from '../dataComponets/favoritesComponents/favoritesMeets.vue'
 export default {
-   components : {redCart , blueCart}
+   data(){
+    return{
+      projects : true , posts : false , meets : false , 
+    }
+   } ,
+   methods : {
+    isProjects(){
+      this.projects = true 
+      this.posts = false 
+      this.meets = false
+    } ,
+    isPosts(){
+      this.projects = false 
+      this.posts = true 
+      this.meets = false
+    } ,
+    isMeets(){
+      this.projects = false 
+      this.posts = false 
+      this.meets = true
+    } ,
+   } ,
+   components : {redCart , blueCart ,favoritesProjects ,favoritesPosts ,favoritesMeets}
 }
 </script>
 
