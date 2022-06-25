7155a168c10b64a4198f40c8f90a4d56e0a5b8b4
ali.mohsenpour <alimpp766@gmail.com>
2022-06-25 10:49:09 -0700

    change in card

--- src/components/cardComponents/blueCart.vue
+++ src/components/cardComponents/blueCart.vue
@@ -3,7 +3,14 @@
     <div class="col-lg-12">
         <div class="blue_cart">
            <span class="light_color size_x">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut cumque exercitationem animi laboriosam voluptatem praesentium...</span>
-           <span class="light_color size_z">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sint dicta earum velit dolore cumque ex recusandae explicabo exercitationem sequi excepturi labore atque molestias, placeat quia sit quaerat deserunt in Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error sit tempore minus</span>
+           <div class="row">
+            <div class="col-lg-6">
+              <span class="light_color size_z">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sint dicta earum velit dolore cumque ex recusandae explicabo exercitationem sequi excepturi labore atque molestias, placeat quia sit quaerat deserunt in</span>
+            </div>
+            <div class="col-lg-6">
+              <h1 class="light_color" style="font-size:100px;">96%</h1>
+            </div>
+           </div>
         </div>
     </div>
   </div>
