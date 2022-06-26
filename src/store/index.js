511e1be74b754279beda75f45773671e269f97ee
ali.mpp <alimpp766@gmail.com>
2022-06-26 15:33:30 +0430

    make Profile

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import Profile from './module/profile'
 import Users from './module/users'
 import Projects from './module/projects'
 import Posts from './module/posts'
@@ -6,6 +7,7 @@ import FavoritesProjects from './favorites/favoritesProjects'
 import FavoritesPosts from './favorites/favoritesPosts'
 export default createStore({
     modules: {
+      Profile : Profile ,
       Users : Users ,
       Projects : Projects , 
       Posts : Posts ,
