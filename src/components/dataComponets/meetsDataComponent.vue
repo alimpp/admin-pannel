45b59a318d094926fb264deb0fe38933234c00f4
ali.mpp <alimpp766@gmail.com>
2022-06-27 12:34:23 +0430

    import image for meets

--- src/components/dataComponets/meetsDataComponent.vue
+++ src/components/dataComponets/meetsDataComponent.vue
@@ -2,17 +2,17 @@
   <div class="meetsData">
      <div class="container">
         <div class="row">
-        <div class="col-lg-6 column mt-2 justify_content">
+        <div class="col-lg-6 column mt-2 justify_content animate__animated animate__fadeInLeft">
            <span class="blue_color size_c">Lorem ipsum, dolor sit amet consectetur...</span>
            <span class="dark_color size_y">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quasi debitis laborum aut soluta molestias magni est. Consequatur dolorem maxime sequi perspiciatis illum, consequuntur quam praesentium vel, fuga eligendi recusandae</span>
         </div>
         <div class="col-lg-6">
-        <redCart class="mt-2"/>
+        <redCart class="mt-2 animate__animated animate__fadeInRight"/>
         </div>
         </div>
         <hr>
       
-        <div class="col-lg-12 my-3" v-for="meet in getMeets" :key="meet.id">
+        <div class="col-lg-12 my-3 animate__animated animate__fadeInRight" v-for="meet in getMeets" :key="meet.id">
             <a :href="`/Meet/${meet.id}`" class="link column">
             <div class="row">
             <div class="col-lg-6 column justify_content">
@@ -27,7 +27,7 @@
                 <span class="dark_color size_y px-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus id quidem vel quo quaerat dignissimos totam inventore libero ex commodi, ea optio. Hic distinctio temporibus ullam numquam quod quia cupiditate!</span>
             </div>
             <div class="col-lg-6">
-            <blueCart/>
+            <img src="./meeting.jpg" alt="Error" style="width:100%; height:300px; border-radius:10px;">
             </div>
             </div>
             </a>
