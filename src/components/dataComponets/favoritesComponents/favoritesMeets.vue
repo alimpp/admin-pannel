a833178d6df808735fe7e3bf229c735e6c17bb21
ali.mpp <alimpp766@gmail.com>
2022-06-28 13:33:28 +0430

    make change

--- src/components/dataComponets/favoritesComponents/favoritesMeets.vue
+++ src/components/dataComponets/favoritesComponents/favoritesMeets.vue
@@ -2,14 +2,14 @@
   <div class="favoritesMeets animate__animated animate__slideInRight">
      <div class="col-lg-12" v-for="meet in getMeets" :key="meet.id">
         <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-6 column justify_content">
                 <div class="d-flex">
-                <span class="blue_color size_y">Meet Name</span>
-                <span class="dark_color size_y px-2">{{meet.name}}</span>
+                    <span class="blue_color size_y">Meet Name</span>
+                    <span class="dark_color size_y px-2">{{meet.name}}</span>
                 </div>
                 <div class="d-flex">
-                  <span class="blue_color size_y">Time Meet</span>
-                  <span class="dark_color size_y px-2">{{meet.time}}</span>
+                    <span class="blue_color size_y">Time Meet</span>
+                    <span class="dark_color size_y px-2">{{meet.time}}</span>
                 </div>
                 <span class="dark_color size_y">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus id quidem vel quo quaerat dignissimos totam inventore libero ex commodi, ea optio. Hic distinctio temporibus ullam numquam quod quia cupiditate!</span>
         </div>
--- src/components/dataComponets/favoritesComponents/favoritesProjects.vue
+++ src/components/dataComponets/favoritesComponents/favoritesProjects.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="favoritesProjects column mt-4 animate__animated animate__slideInRight">
    <div class="row">
-    <div class="col-lg-4 " v-for="project in getFavoritesProjects" :key="project.id">
-       <a :href="`/Projects/${project.id}`" class="link column">
+    <div class="col-lg-12 " v-for="project in getFavoritesProjects" :key="project.id">
+       <div class="row">
+      <div class="col-lg-6">
+      <a :href="`/Projects/${project.id}`" class="link column justify_content mt-5">
        <span class="blue_color size_x">Project Name</span>
        <span class="dark_color size_y py-1 px-1 bg-light">{{project.name}}</span>
        <span class="blue_color size_x">Create Time</span>
@@ -10,6 +12,11 @@
        <span class="blue_color size_x">Create By</span>
        <span class="dark_color size_y py-1 px-1 bg-light">{{project.createBy}}</span>
        </a>
+       </div>
+       <div class="col-lg-6">
+       <img src="./project.jpeg" alt="Error" style="width:100%; height:300px;">
+       </div>
+       </div>
        <hr>
     </div>
    </div>
