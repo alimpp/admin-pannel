12b1cff45b8a92844d98eb7de351031ff83f03d1
ali.mpp <alimpp766@gmail.com>
2022-06-29 10:27:58 +0430

    fix flex

--- src/components/dataComponets/favoritesComponents/favoritesMeets.vue
+++ src/components/dataComponets/favoritesComponents/favoritesMeets.vue
@@ -2,7 +2,9 @@
   <div class="favoritesMeets animate__animated animate__slideInRight">
      <div class="col-lg-12" v-for="meet in getMeets" :key="meet.id">
         <div class="row">
-        <div class="col-lg-6 column justify_content">
+     
+        <div class="col-lg-6 ">
+        <a :href="`/meet/${meet.id}`" class="link column justify_content mt-5">
                 <div class="d-flex">
                     <span class="blue_color size_y">Meet Name</span>
                     <span class="dark_color size_y px-2">{{meet.name}}</span>
@@ -12,7 +14,9 @@
                     <span class="dark_color size_y px-2">{{meet.time}}</span>
                 </div>
                 <span class="dark_color size_y">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus id quidem vel quo quaerat dignissimos totam inventore libero ex commodi, ea optio. Hic distinctio temporibus ullam numquam quod quia cupiditate!</span>
+        </a>
         </div>
+      
         <div class="col-lg-6">
         <img src="./meeting.jpg" alt="Error" style="width:100%; height:300px;">
         </div>
